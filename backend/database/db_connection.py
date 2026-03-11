3610a2f1f01afdb2e5c14607ac6b48a191767156
d-i-ksha <dikshasaklani097@gmail.com>
2026-03-11 09:40:42 +0530

    Database checked

--- backend/database/db_connection.py
+++ backend/database/db_connection.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="YOUR_DB_PASSWORD",
+        password="diKSha@07#",
         database="dashdeal"
     )
     return connection
