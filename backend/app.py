d3f26de68f2853fb5cbabd5f98516c81a735c050
d-i-ksha <dikshasaklani097@gmail.com>
2026-03-11 09:22:50 +0530

    Final changes

--- backend/app.py
+++ backend/app.py
@@ -19,5 +19,4 @@ def home():
     return "DashDeal Backend Running 🚀"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://dashdeal.onrender.com";
+const BASE_URL = "http://127.0.0.1:5000";
 let localInventory = []; 
 
 // 1. Unified Page Loader
