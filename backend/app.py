22a55a499dc464a5b70c4df88e4fb4e5b9d7567b
d-i-ksha <dikshasaklani097@gmail.com>
2026-08-05 15:22:21 +0530

    Fix static file routes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes.user_routes import user_routes
 from routes.product_routes import product_routes
@@ -16,7 +16,39 @@ app.register_blueprint(order_routes,url_prefix='/orders')
 
 @app.route("/")
 def home():
-    return "DashDeal Backend Running 🚀"
+    return send_from_directory("../frontend/html","index.html")
+
+@app.route("/product")
+def product():
+    return send_from_directory("../frontend/html", "product.html")
+
+@app.route("/cart")
+def cart():
+    return send_from_directory("../frontend/html", "cart.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return send_from_directory("../frontend/html", "dashboard.html")
+
+@app.route("/login")
+def login():
+    return send_from_directory("../frontend/html", "login.html")
+
+@app.route("/checkout")
+def checkout():
+    return send_from_directory("../frontend/html", "checkout.html")
+
+@app.route("/payment")
+def payment():
+    return send_from_directory("../frontend/html", "payment.html")
+
+@app.route("/css/<path:filename>")
+def css(filename):
+    return send_from_directory("../frontend/css", filename)
+
+@app.route("/js/<path:filename>")
+def js(filename):
+    return send_from_directory("../frontend/js", filename)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
